68774c01ffccea5e1996431f983d5392dcd7a082
jackground <jackground@icloud.com>
2015-10-03 19:12:11 -0700

    initial commit

--- Algorhythm/ViewController.swift
+++ Algorhythm/ViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var aButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        aButton.setTitle("Press me!", forState: .Normal)
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +24,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func buttonPressed() {
+        view.backgroundColor = UIColor.orangeColor()
+    }
 
 }
 
